a5a143d7efeb9a3b2d1216cfc6a5391a0bb3af10
Mayo <mayo@linux.com>
2025-04-13 05:23:41 +0900

    feat: update dependencies and enhance model session handling in main.rs

--- comic-text-detector/src/main.rs
+++ comic-text-detector/src/main.rs
@@ -1,5 +1,10 @@
 use clap::Parser;
-use ort::{Environment, SessionBuilder};
+use image::GenericImageView;
+use ndarray::Array;
+use ort::{
+    inputs,
+    session::{Session, builder::GraphOptimizationLevel},
+};
 
 #[derive(Parser)]
 struct Args {
@@ -13,15 +18,31 @@ struct Args {
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    let env = std::sync::Arc::new(
-        Environment::builder()
-            .with_name("comic-text-detector")
-            .build()?,
-    );
+    let model = Session::builder()?
+        .with_optimization_level(GraphOptimizationLevel::Level3)?
+        .with_intra_threads(4)?
+        .commit_from_file(args.model)?;
 
-    let session = SessionBuilder::new(&env)?.with_model_from_file(&args.model)?;
+    println!("loaded model: {model:?}");
 
-    println!("loaded model: {session:?}");
+    let image =
+        image::open(&args.image).map_err(|e| anyhow::anyhow!("Failed to open image: {e}"))?;
+
+    let image = image.resize_exact(1024, 1024, image::imageops::FilterType::CatmullRom);
+    let mut input = Array::zeros((1, 3, 1024, 1024));
+    for pixel in image.pixels() {
+        let x = pixel.0 as _;
+        let y = pixel.1 as _;
+        let [r, g, b, _] = pixel.2.0;
+        input[[0, 0, y, x]] = (r as f32) / 255.;
+        input[[0, 1, y, x]] = (g as f32) / 255.;
+        input[[0, 2, y, x]] = (b as f32) / 255.;
+    }
+
+    let inputs = inputs!["images" => input.view()]?;
+    let outputs = model.run(inputs)?;
+
+    println!("outputs: {outputs:?}");
 
     Ok(())
 }
