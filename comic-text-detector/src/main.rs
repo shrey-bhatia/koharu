2f334e5522100f98513f33398e626b2530a563a9
Mayo <mayo@linux.com>
2025-04-13 17:22:43 +0900

    feat: add image processing and object detection functionality to comic-text-detector

--- comic-text-detector/src/main.rs
+++ comic-text-detector/src/main.rs
@@ -1,3 +1,4 @@
+use candle_transformers::object_detection::{Bbox, non_maximum_suppression};
 use clap::Parser;
 use image::GenericImageView;
 use ndarray::Array;
@@ -25,10 +26,14 @@ fn main() -> anyhow::Result<()> {
 
     println!("loaded model: {model:?}");
 
-    let image =
+    let orig_image =
         image::open(&args.image).map_err(|e| anyhow::anyhow!("Failed to open image: {e}"))?;
+    let (orig_width, orig_height) = orig_image.dimensions();
+    let w_ratio = orig_width as f32 / 1024 as f32;
+    let h_ratio = orig_height as f32 / 1024 as f32;
 
-    let image = image.resize_exact(1024, 1024, image::imageops::FilterType::CatmullRom);
+    let image = orig_image.resize_exact(1024, 1024, image::imageops::FilterType::CatmullRom);
+
     let mut input = Array::zeros((1, 3, 1024, 1024));
     for pixel in image.pixels() {
         let x = pixel.0 as _;
@@ -42,7 +47,51 @@ fn main() -> anyhow::Result<()> {
     let inputs = inputs!["images" => input.view()]?;
     let outputs = model.run(inputs)?;
 
-    println!("outputs: {outputs:?}");
+    let blk = outputs["blk"].try_extract_tensor::<f32>()?;
+    let blk = blk.view();
+
+    let mut boxes: Vec<Vec<Bbox<_>>> = (0..2).map(|_| vec![]).collect();
+    for i in 0..blk.shape()[1] {
+        let confidence = blk[[0, i, 4]];
+        if confidence < 0.25 {
+            continue;
+        }
+
+        let mut class_index = 0;
+        if blk[[0, i, 5]] > blk[[0, i, 6]] {
+            class_index = 1;
+        }
+
+        let center_x = blk[[0, i, 0]] * w_ratio;
+        let center_y = blk[[0, i, 1]] * h_ratio;
+        let width = blk[[0, i, 2]] * w_ratio;
+        let height = blk[[0, i, 3]] * h_ratio;
+
+        boxes[class_index].push(Bbox {
+            confidence,
+            xmin: center_x - width / 2.,
+            ymin: center_y - height / 2.,
+            xmax: center_x + width / 2.,
+            ymax: center_y + height / 2.,
+            data: (),
+        });
+    }
+
+    non_maximum_suppression(&mut boxes, 0.45);
+
+    for (i, box_list) in boxes.iter().enumerate() {
+        for bbox in box_list {
+            let x0 = bbox.xmin as u32;
+            let y0 = bbox.ymin as u32;
+            let x1 = bbox.xmax as u32;
+            let y1 = bbox.ymax as u32;
+
+            println!(
+                "Class: {i}, Confidence: {:.2}, Box: ({x0}, {y0}), ({x1}, {y1})",
+                bbox.confidence
+            );
+        }
+    }
 
     Ok(())
 }
