5e6c21ac7eae8cac1c955f37de604ecf78763b6a
Mayo <mayo@linux.com>
2025-04-15 01:28:17 +0900

    feat: integrate react-konva for canvas rendering in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Layer, Stage, Text } from 'react-konva'
 import LayerPanel from './components/layer-panel'
 import Tools from './components/tools'
 import Topbar from './components/topbar'
@@ -9,7 +10,13 @@ function App() {
       <div className='flex flex-1 my-2'>
         <Tools />
 
-        <div className='flex-1 flex items-center justify-center'></div>
+        <div className='flex-1 flex items-center justify-center'>
+          <Stage width={200} height={200} className='bg-white'>
+            <Layer>
+              <Text text='Hello, World!' fontSize={30} fill='black' />
+            </Layer>
+          </Stage>
+        </div>
 
         <div className='h-full'>
           <LayerPanel />
