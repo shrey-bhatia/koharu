a128b74a933f4841ef947fd7f065ad372ac3ef9e
Mayo <mayo@linux.com>
2025-04-18 21:57:40 +0900

    remove storage for now

--- next/components/canvas.tsx
+++ next/components/canvas.tsx
@@ -3,9 +3,7 @@
 import Konva from 'konva'
 import { useEffect, useRef } from 'react'
 import { useStageStore } from '@/lib/state'
-import { storage } from '@/lib/storage'
 import { debug } from '@tauri-apps/plugin-log'
-import { initializeStageWithImage } from '@/lib/stage'
 import ScaleControl from './scale-control'
 
 function Canvas() {
@@ -14,19 +12,11 @@ function Canvas() {
   const { setStage } = useStageStore()
 
   const initializeStage = async () => {
-    const serialized = await storage.get<string | null>('stage')
-    let stage: Konva.Stage
-    if (serialized) {
-      stage = Konva.Node.create(serialized, stageRef.current)
-      initializeStageWithImage(stage, stage.getAttr('image'))
-      debug('Restored stage from storage')
-    } else {
-      stage = new Konva.Stage({
-        container: stageRef.current,
-        width: containerRef.current?.offsetWidth,
-        height: containerRef.current?.offsetHeight,
-      })
-    }
+    const stage = new Konva.Stage({
+      container: stageRef.current,
+      width: containerRef.current?.offsetWidth,
+      height: containerRef.current?.offsetHeight,
+    })
 
     setStage(stage)
     debug(`Stage initialized`)
--- next/components/topbar.tsx
+++ next/components/topbar.tsx
@@ -1,11 +1,10 @@
 'use client'
 
 import { open } from '@tauri-apps/plugin-dialog'
-import { Save, Image, Download } from 'lucide-react'
+import { Image, Download } from 'lucide-react'
 import { debug } from '@tauri-apps/plugin-log'
 import { convertFileSrc } from '@tauri-apps/api/core'
 import { useStageStore } from '@/lib/state'
-import { storage } from '@/lib/storage'
 import { initializeStageWithImage } from '@/lib/stage'
 
 function Topbar() {
@@ -32,12 +31,6 @@ function Topbar() {
     initializeStageWithImage(stage, imageUrl)
   }
 
-  const handleSave = async () => {
-    const serialized = stage.toJSON()
-    await storage.set('stage', serialized)
-    debug(`Saved stage to storage, size: ${serialized}`)
-  }
-
   return (
     <div className='flex items-center p-2 bg-white border-b border-gray-200 shadow-sm'>
       <div className='flex items-center'>
@@ -46,13 +39,6 @@ function Topbar() {
           onClick={handleOpenFile}
         >
           <Image size={18} />
-        </button>
-
-        <button
-          className='flex items-center p-2 mx-1 text-gray-600 hover:bg-gray-100 rounded'
-          onClick={handleSave}
-        >
-          <Save size={18} />
         </button>
       </div>
 
