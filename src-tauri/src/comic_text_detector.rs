91beeeb32e81a67a68cfefad5b3627ecf21e604c
Mayo <mayo@linux.com>
2025-04-20 09:13:16 +0900

    feat: add comic text detection functionality and update dependencies

--- src-tauri/src/comic_text_detector.rs
+++ src-tauri/src/comic_text_detector.rs
@@ -4,12 +4,10 @@ use ort::session::Session;
 #[derive(Debug)]
 pub struct ComicTextDetector {
     model: Session,
-    confidence_threshold: f32,
-    nms_threshold: f32,
 }
 
 impl ComicTextDetector {
-    pub fn new(confidence_threshold: f32, nms_threshold: f32) -> anyhow::Result<Self> {
+    pub fn new() -> anyhow::Result<Self> {
         let api = Api::new()?;
         let repo = api.model("mayocream/koharu".to_string());
         let model_path = repo.get("comictextdetector.onnx")?;
@@ -19,10 +17,6 @@ impl ComicTextDetector {
             .with_intra_threads(4)?
             .commit_from_file(model_path)?;
 
-        Ok(ComicTextDetector {
-            model,
-            confidence_threshold,
-            nms_threshold,
-        })
+        Ok(ComicTextDetector { model })
     }
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,46 @@
+use tokio::sync::Mutex;
+
+use tauri::{AppHandle, Manager, async_runtime::spawn};
+
+mod comic_text_detector;
+
+#[derive(Default)]
+struct AppState {
+    ctd: Option<comic_text_detector::ComicTextDetector>,
+}
+
+async fn initialize(app: AppHandle) -> anyhow::Result<()> {
+    let state = app.state::<Mutex<AppState>>();
+    let mut state = state.lock().await;
+    state.ctd = Some(comic_text_detector::ComicTextDetector::new()?);
+
+    // hide the splash screen
+    if let Some(splashscreen) = app.get_webview_window("splashscreen") {
+        splashscreen.close()?;
+    }
+
+    // show the main window
+    if let Some(window) = app.get_webview_window("main") {
+        window.show()?;
+    }
+
+    Ok(())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .manage(Mutex::new(AppState::default()))
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_persisted_scope::init())
         .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            spawn(initialize(app.handle().clone()));
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
