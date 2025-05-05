1c77deb4175092baf370b1130de0818707f25c71
Mayo <mayo@linux.com>
2025-05-05 12:30:04 +0900

    feat: add notification

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,4 @@
+use tauri_plugin_notification::NotificationExt;
 use tokio::sync::RwLock;
 
 use tauri::{AppHandle, Manager, async_runtime::spawn};
@@ -82,6 +83,7 @@ async fn inpaint(
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() -> anyhow::Result<()> {
     tauri::Builder::default()
+        .plugin(tauri_plugin_notification::init())
         .manage(RwLock::new(AppState::default()))
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(
@@ -94,7 +96,21 @@ pub fn run() -> anyhow::Result<()> {
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
             // initialize the app state
-            spawn(initialize(app.handle().clone()));
+            let app_handle = app.handle().clone();
+            spawn({
+                async move {
+                    if let Err(e) = initialize(app_handle.clone()).await {
+                        app_handle
+                            .notification()
+                            .builder()
+                            .title("Error")
+                            .body(format!("Failed to initialize: {}", e))
+                            .show()
+                            .unwrap();
+                        app_handle.exit(1);
+                    }
+                }
+            });
 
             Ok(())
         })
