48443b6a4688cff4aa491eba6e1560187afa7878
Mayo <mayo@linux.com>
2025-05-29 05:02:17 +0900

    ci: fix windows bundle

--- next/next.config.ts
+++ next/next.config.ts
@@ -4,24 +4,6 @@ const nextConfig: NextConfig = {
   distDir: 'dist',
   output: 'export',
   reactStrictMode: false,
-  // refer: https://web.dev/articles/cross-origin-isolation-guide
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-        ],
-      },
-    ]
-  },
 }
 
 export default nextConfig
