18fdf310518186bb6d41a7334820bb894c03271e
Mayo <mayo@linux.com>
2025-08-06 14:29:23 +0900

    initialize models in tauri

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,30 @@
+use comic_text_detector::ComicTextDetector;
+use lama::Lama;
+use manga_ocr::MangaOCR;
 use tauri_plugin_notification::NotificationExt;
 use tokio::sync::RwLock;
 
 use tauri::{AppHandle, Manager, async_runtime::spawn};
 
-#[derive(Default)]
-struct AppState {}
+#[derive(Debug)]
+struct AppState {
+    comic_text_detector: ComicTextDetector,
+    manga_ocr: MangaOCR,
+    lama: Lama,
+}
 
+// Initialize models
 async fn initialize(app: AppHandle) -> anyhow::Result<()> {
+    let comic_text_detector = ComicTextDetector::new()?;
+    let manga_ocr = MangaOCR::new()?;
+    let lama = Lama::new()?;
+
+    app.manage(RwLock::new(AppState {
+        comic_text_detector,
+        manga_ocr,
+        lama,
+    }));
+
     app.get_webview_window("splashscreen").unwrap().close()?;
     app.get_webview_window("main").unwrap().show()?;
 
@@ -18,7 +36,6 @@ pub fn run() -> anyhow::Result<()> {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_notification::init())
-        .manage(RwLock::new(AppState::default()))
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
